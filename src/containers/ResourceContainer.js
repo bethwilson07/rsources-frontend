dfa03d91e8fa5c872e41ea1080e3d77336e0a02e
bethwilson07 <beth.ny.wlsn@gmail.com>
2019-01-23 12:48:03 -0500

    added pictures to resource container

--- src/containers/ResourceContainer.js
+++ src/containers/ResourceContainer.js
@@ -6,11 +6,11 @@ import {Link, withRouter} from 'react-router-dom'
 class ResourceContainer extends React.Component {
 
   resourceTypes() {
-    const types = [{id: 1, name:"Project", title:"Project Ideas", photo: "http://icons-for-free.com/free-icons/png/512/2315596.png"},
-    {id: 2, name: "Review_Activity", title: "Review Activities", photo: "http://icons-for-free.com/free-icons/png/512/2315596.png"},
-    {id: 3, name: "Lab", title: "Labs", photo: "http://icons-for-free.com/free-icons/png/512/2315596.png"},
-    {id: 4, name: "Field_Trip_Idea", title: "Field Trip Ideas", photo: "http://icons-for-free.com/free-icons/png/512/2315596.png"},
-    {id: 5, name: "Website", title: "Helpful Websites", photo: "http://icons-for-free.com/free-icons/png/512/2315596.png"}]
+    const types = [{id: 1, name:"Project", title:"Project Ideas", photo: "https://i.pinimg.com/originals/11/ad/65/11ad6535313c4c197dfa9ff711866641.png"},
+    {id: 2, name: "Review_Activity", title: "Review Activities", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSb8sKPHBk7x5qFSXwW_Wf4vEZGBq9QqWEtxECSr3sjwdbZMdP"},
+    {id: 3, name: "Lab", title: "Labs", photo: "https://cdn.dribbble.com/users/143127/screenshots/2460438/january-15.png"},
+    {id: 4, name: "Field_Trip_Idea", title: "Field Trip Ideas", photo: "https://safewaybus.files.wordpress.com/2012/12/school-bus-clip-art-21.jpg"},
+    {id: 5, name: "Website", title: "Helpful Websites", photo: "http://worldartsme.com/images/cartoon-laptop-computer-clipart-1.jpg"}]
     return types;
   }
 
@@ -20,7 +20,11 @@ class ResourceContainer extends React.Component {
         <Grid>
           <Grid.Row className="sources" columns={3}>
             {this.resourceTypes().map(t => <Link to={`/course/${this.props.courseId}/resources/${t.name}`} key={t.id} >
-            <ResourceCard key={t.id} title={t.title} photo={t.photo}/></Link>)}
+            <ResourceCard key={t.id} title={t.title} photo={t.photo}/></Link>).slice(0,3)}
+          </Grid.Row>
+          <Grid.Row className="sources" columns={2}>
+            {this.resourceTypes().map(t => <Link to={`/course/${this.props.courseId}/resources/${t.name}`} key={t.id} >
+            <ResourceCard key={t.id} title={t.title} photo={t.photo}/></Link>).slice(3)}
           </Grid.Row>
         </Grid>
       </div>
